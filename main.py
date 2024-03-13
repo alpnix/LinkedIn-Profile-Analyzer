9f8a712ded0dd17f9dc1b924b1d9160b6f2c0515
Delario4994 <Delarionancejr@gmail.com>
2024-03-12 20:01:38 -0400

    wrote prompt to user for HTML link

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        url = request.form.get('url')
+        return render_template('index.html', url=url)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
