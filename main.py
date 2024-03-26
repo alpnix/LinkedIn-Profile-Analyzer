29322f1adf280d58e6fd1200cafa3ba3696e329c
Delario4994 <Delarionancejr@gmail.com>
2024-03-26 12:54:42 -0400

    removed POST from index view function

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    if request.method == 'POST':
-        url = request.form.get('url')
-        return render_template('index.html', url=url)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5500)
